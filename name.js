0e21c9ca5653cfbe16231e3c5399593d121150c3
Noel Ryan <noelgerard.ryan@gmail.com>
2020-02-20 13:43:26 +0100

    Updating the javascripts excercises

--- name.js
+++ name.js
@@ -1,8 +1,7 @@
 myArray = ["Noel", "Sarah", "Peter"];
 emptyArray = []
 
-for (let i = 0; i <= myArray.length; i++){
+for (let i = 0; i < myArray.length; i++){
     emptyArray.push(myArray [i]);
 }
 
-console.log (emptyArray);
